6ff88260f4f7aff2ac463c98928cf268674a441a
K-Schaeffer <kcb.schaeffer@gmail.com>
2020-07-02 15:15:06 -0300

    Loading all elements that will be used

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,9 +1,28 @@
+/**
+ * CLASS PURPOSE: Deal with the intelligence of the project
+ * DEVELOPER: Kauan Schaeffer, HCODE Instructors
+ */
 class WhatsAppController {
 
     constructor(){
 
         console.log('Working');
+
+        this.loadElements();
         
+
+    }
+
+    // Load all id's from HTML and create a object with each id
+    loadElements(){
+
+        this.el = {};
+
+        document.querySelectorAll('[id]').forEach(element => {
+
+            this.el[Format.toCamelCase(element.id)] = element;
+            
+        });
 
     }
 
--- src/util/Format.js
+++ src/util/Format.js
@@ -1,4 +1,19 @@
-class Format{
+/**
+ * CLASS PURPOSE: Format text, number, currency
+ * DEVELOPER: Kauan Schaeffer, HCODE Instructors
+ */
+class Format {
 
-    
+    // Format the id's to CamelCase
+    static toCamelCase(text) {
+
+        // Mandatory div so I can create the dataset
+        let div = document.createElement('div');
+        div.innerHTML = `<div data-${text}="id"></div>`;
+
+        return Object.keys(div.firstChild.dataset)[0];
+
+    }
+
+
 }
